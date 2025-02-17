054824a8082a5aa4433507f4a4051fa52e909265
Brooklyn Harden <bharden@udel.edu>
2025-02-17 09:51:19 -0500

    created the class and created a board function

--- main.ts
+++ main.ts
@@ -1,6 +1,38 @@
-//set up board
-let board: string[] = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+class TTT {
+    private board:string[];
+    private currentPlayer: string;
+    private isGameOver: boolean;
 
-function printBoard(){
-    console.l
-}   
+    constructor(){
+        this.board = new Array(9);
+        for(let i=0; i<9; i++){
+            this.board[i] = "";
+        }
+
+        this.currentPlayer= "X";
+        this.isGameOver = false;
+        this.createBoard();
+    }
+
+    private createBoard():void{
+        const boardElement = document.getElementById("board") as HTMLDivElement;
+
+        //CLEARS any existing content in the board
+        boardElement.innerHTML = "";
+
+        for(let i=0; i< this.board.length;i++){
+
+            //creating a new box (div) for each cell
+            const cell = document.createElement("div");
+
+            //giving it a class name to edit later
+            cell.classList.add("cell");
+
+            //stores the number of cell so we know its position
+            cell.dataset.index = i.toString();
+
+            //adding the cells to the board
+            boardElement.appendChild(cell);
+        }
+    }
+}
